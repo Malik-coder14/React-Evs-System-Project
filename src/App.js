39a2ba88c5b2f63505793e7d46a98ea5c2c54747
Malikcoder-14 <malik.eng5@gmail.com>
2025-08-18 17:13:22 +0500

    React Project EVS System

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Home from "./components/Home";
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Products from "./components/Products";
+import Header from "./components/includes/Header";
+import Test from "./components/Test";
+
+ function App() {
+    return (
+        <div>
+            
+            <Header/>
+        
+            <div className = "container">
+                 <Routes>
+                <Route path= "/" element = {<Home/>}/>
+                <Route path= "/about" element = {<About/>}/>
+                <Route path= "/products" element = {<Products/>}/>
+                <Route path= "/contact" element = {<Contact/>}/>
+                <Route path = "/test" element = {<Test/>} />
+           </Routes>   
+            </div>         
+          
+
+         </div>
+
+    );
+
+}
+export default App;
